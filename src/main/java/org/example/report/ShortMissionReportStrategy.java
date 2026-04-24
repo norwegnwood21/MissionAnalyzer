4961ce5d014a6b8bb6b987392e94ab3036b4a0f2
Liliya Merzlyakova <norwegnwood@mail.ru>
2026-04-24 22:35:40 +0300

    3rd laba second commit

--- src/main/java/org/example/report/ShortMissionReportStrategy.java
+++ src/main/java/org/example/report/ShortMissionReportStrategy.java
@@ -3,6 +3,6 @@ import org.example.model.Mission;
 public class ShortMissionReportStrategy implements MissionReportStrategy {
     @Override
     public String generate(Mission mission) {
-        return "Миссия " + mission.getMissionId() + " | " + mission.getDate() + " | " + mission.getOutcome() + " | " + mission.getLocation();
+        return "Миссия  " + mission.getMissionId() + " | " + mission.getDate() + " | " + mission.getOutcome() + " | " + mission.getLocation();
     }
 }
